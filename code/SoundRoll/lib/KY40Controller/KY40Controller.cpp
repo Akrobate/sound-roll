9843f5466e72eca61e42650aa3eb39977a317a47
Akrobate <fedorov.artiom@gmail.com>
2024-11-05 16:28:42 +0100

    Scaffolded class for KY40 controller

---
+++ code/SoundRoll/lib/KY40Controller/KY40Controller.cpp
@@ -0,0 +1,5 @@
+#include "KY40Controller.h"
+
+KY40Controller::KY40Controller() {
+    this->value = 0;
+}
---
+++ code/SoundRoll/lib/KY40Controller/KY40Controller.h
@@ -0,0 +1,14 @@
+#ifndef KY40Controller_h
+#define KY40Controller_h
+
+class KY40Controller {
+
+    private:
+        int value = 0;
+
+    public:
+        KY40Controller();
+        void update();
+};
+
+#endif
