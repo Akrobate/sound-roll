f7502623b61eca49dee127d62c5be571be954dce
Akrobate <fedorov.artiom@gmail.com>
2024-11-09 19:59:54 +0100

    Working on server

--- code/SoundRoll/lib/Server/Server.cpp
+++ code/SoundRoll/lib/Server/Server.cpp
@@ -6,8 +6,27 @@ Server::Server() {
 
 Server::Server(int port) {
     this->port = port;
+    this->server = new AsyncWebServer(this->port);
+    this->server->begin();
+
 }
 
 void Server::init() {
     
+  this->server->onNotFound(
+    [](AsyncWebServerRequest * request){
+      request->send(404, "text/html", "Not found");
+    }
+  );
+
+
+  this->server->on(
+    "/",
+    HTTP_GET,
+    [](AsyncWebServerRequest * request){
+        string index_html = "<!DOCTYPE html><html><head><title>SoundRoll</title></head><body><h1>SoundRoll</h1></body></html>";
+        request->send_P(200, "text/html", index_html);
+    }
+  );
+
 }
--- code/SoundRoll/lib/Server/Server.h
+++ code/SoundRoll/lib/Server/Server.h
@@ -1,11 +1,15 @@
 #ifndef Server_h
 #define Server_h
 
+#include <ESPAsyncWebServer.h>
+#include "AsyncJson.h"
+#include "ArduinoJson.h"
+
 class Server {
 
     private:
         int port = 0;
-
+        AsyncWebServer * server;
 
     public:
         Server();
