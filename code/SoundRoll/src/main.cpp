760b642a5097a8387f1a83fd36def5f7e56fc9ca
Akrobate <fedorov.artiom@gmail.com>
2024-11-11 18:28:29 +0100

    Usage of server in main

--- code/SoundRoll/src/main.cpp
+++ code/SoundRoll/src/main.cpp
@@ -1,11 +1,17 @@
 #include <Arduino.h>
 
 #include <KY40Controller.h>
+#include <Server.h>
+
+
+#define SERVER_PORT 80
 
 KY40Controller * ly_40_controller = new KY40Controller();
+Server * server = new Server(SERVER_PORT);
+
 
 void setup() {
-
+    server->begin();
 }
 
 void loop() {
