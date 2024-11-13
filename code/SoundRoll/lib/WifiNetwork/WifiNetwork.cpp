34c66574191e215257c66986f5cc1d13d858662b
Akrobate <fedorov.artiom@gmail.com>
2024-11-13 18:38:23 +0100

    Working on wifi layer

--- code/SoundRoll/lib/WifiNetwork/WifiNetwork.cpp
+++ code/SoundRoll/lib/WifiNetwork/WifiNetwork.cpp
@@ -7,3 +7,20 @@ WifiNetwork::WifiNetwork() {
 void WifiNetwork::init() {
 
 }
+
+void WifiNetwork::setSsid(String ssid) {
+    this->ssid = ssid;
+}
+
+void WifiNetwork::setPassword(String password) {
+    this->password = password;
+}
+
+String WifiNetwork::getSsid() {
+    return this->ssid;
+}
+
+String WifiNetwork::getPassword() {
+    return this->password;
+}
+
--- code/SoundRoll/lib/WifiNetwork/WifiNetwork.h
+++ code/SoundRoll/lib/WifiNetwork/WifiNetwork.h
@@ -1,6 +1,7 @@
 #ifndef WifiNetwork_h
 #define WifiNetwork_h
 
+#include "Arduino.h"
 #include <ESPAsyncWebServer.h>
 #include "AsyncJson.h"
 #include "ArduinoJson.h"
@@ -8,10 +9,21 @@
 class WifiNetwork {
 
     private:
-        
+        String ssid;
+        String password;
+
     public:
         Server();
         void init();
+
+        setSsid(String ssid);
+        setPassword(String password);
+
+        String getSsid();
+        String getPassword();
+
+
+
 };
 
 #endif
