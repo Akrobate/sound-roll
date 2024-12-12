834030a36b4a121c3d4b0664db5527ed6e8ba2fa
Akrobate <fedorov.artiom@gmail.com>
2024-12-12 20:34:34 +0100

    Baudrate become a constante

--- code/SoundRoll/src/main.cpp
+++ code/SoundRoll/src/main.cpp
@@ -6,13 +6,14 @@
 
 
 #define SERVER_PORT 80
+#define SERIAL_BAUD 115200
 
 KY40Controller * ky_40_controller = new KY40Controller();
 Server * server = new Server(SERVER_PORT);
 WifiNetwork * wifi_network = new WifiNetwork();
 
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(SERIAL_BAUD);
     wifi_network->init();
     ky_40_controller->init();
     server->begin();
