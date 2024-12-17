429a80f13b144d5e4a45019047482ae9d699c3dd
Akrobate <fedorov.artiom@gmail.com>
2024-12-17 23:59:32 +0100

    Updated business state

--- code/SoundRoll/lib/BusinessState/BusinessState.cpp
+++ code/SoundRoll/lib/BusinessState/BusinessState.cpp
@@ -1,4 +1,4 @@
-'use strict';
+#include "BusinessState.h"
 
 BusinessState::BusinessState() {
     
--- code/SoundRoll/lib/BusinessState/BusinessState.h
+++ code/SoundRoll/lib/BusinessState/BusinessState.h
@@ -5,12 +5,18 @@ class BusinessState {
 
     private:
         unsigned int volume;
+        String configuration_file = "/configurations.json";
+        String wifi_password = "PASSWORD";
+        String wifi_ssid = "PASSWORD";
+        
 
     public:
         BusinessState();
 
         void setVolume(unsigned int volume);
         unsigned int getVolume();
+
+        void init();
 
 };
 
